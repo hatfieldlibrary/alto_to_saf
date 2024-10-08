d22877b7ba1d5a8443dab1346bdbe1180c5d6a93
Michael Spalti <mspalti@gmail.com>
2024-10-08 12:26:35 -0700

    Updted args input

--- read_input.py
+++ read_input.py
@@ -3,17 +3,17 @@ import argparse
 from processor.process_data import process_files
 
 parser = argparse.ArgumentParser(description='Process METS/ALTO data for DSpace simple archive format import.')
-parser.add_argument('input_dir', metavar='input directory', type=str,
-                    help='the parent directory containing METS, ALTO, image files and PDF')
-parser.add_argument('saf', metavar='Output SAF directory', type=str,
-                    help='full path to the output directory that will contain the SAF subdirectories')
-parser.add_argument('config', metavar='metadata configuration', nargs='?', type=str, default='default',
-                    help='Metadata configuration file: current options are collegian, bulletin')
+parser.add_argument('input_dir', metavar='Input directory', type=str,
+                    help='The parent directory containing METS, ALTO, image files and PDF')
+parser.add_argument('saf', metavar='Output directory', type=str,
+                    help='Full path to the output directory for the processed SAF subdirectories')
+parser.add_argument('config', metavar='Metadata configuration', nargs='?', type=str, default='default',
+                    help='Metadata configuration file (optional). Current configurations are "collegian", "bulletin"')
 parser.add_argument('-b',  "--bundle",
-                    help='images can be added to an alternate bundle if you do not want them included in the default '
+                    help='Images can be added to an alternate bundle if you do not want them included in the default '
                          '(ORIGINAL) bundle')
 parser.add_argument('-s',  "--strip",
-                    help='if provided this value will be stripped from the beginning of file names, e.g.'
+                    help='If provided this value will be stripped from the beginning of file names, e.g.'
                          'Page_001.xml will be converted to 001.xml if the value "Page_" is provided')
 
 args = parser.parse_args()

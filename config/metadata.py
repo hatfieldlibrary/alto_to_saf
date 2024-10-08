964d0b317f522e881e3401516ec307a85840a698
Michael Spalti <mspalti@gmail.com>
2024-10-08 11:27:52 -0700

    Added default metadata configuration

--- config/metadata.py
+++ config/metadata.py
@@ -1,5 +1,21 @@
 # Metadata for Collegian issues
 collection_metadata_configs = {
+    'default':{
+        'title': None,
+        # If provided the subtitle is concatenated with the title. E.g. Willamette Collegian (Salem, OR), 1925-02-11
+        # In this example, the subtitle is the date.
+        'sub_title': None,
+        'alt_title': None,
+        'date_issued': None,
+        'date_created': None,
+        'description': None,
+        'volume': None,
+        'issue': None,
+        'dimensions': None,
+        'type': None,
+        'publisher': None,
+        'rights': None
+    },
     'collegian': {
         'title': 'Willamette Collegian (Salem, OR)',
         # If provided the subtitle is concatenated with the title. E.g. Willamette Collegian (Salem, OR), 1925-02-11
--- read_input.py
+++ read_input.py
@@ -7,8 +7,8 @@ parser.add_argument('input_dir', metavar='input directory', type=str,
                     help='the parent directory containing METS, ALTO, image files and PDF')
 parser.add_argument('saf', metavar='Output SAF directory', type=str,
                     help='full path to the output directory that will contain the SAF subdirectories')
-parser.add_argument('config', metavar='metadata configuration', type=str,
-                    help='Metadata configuration file: current options are collegain, bulletin')
+parser.add_argument('config', metavar='metadata configuration', nargs='?', type=str, default='default',
+                    help='Metadata configuration file: current options are collegian, bulletin')
 parser.add_argument('-b',  "--bundle",
                     help='images can be added to an alternate bundle if you do not want them included in the default '
                          '(ORIGINAL) bundle')
